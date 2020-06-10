18cae1dd2b2ab738f1a90102e23a9f4a68b422d6
Aoi Uchino <aoi.uchino.ct@nttdocomo.com>
2020-06-10 14:40:52 +0900

    add version2

--- src/main/java/la/servlet/PlusServlet.java
+++ src/main/java/la/servlet/PlusServlet.java
@@ -44,15 +44,39 @@ public class PlusServlet extends HttpServlet {
 
 		String num1 = request.getParameter("value1");
 		String num2 = request.getParameter("value2");
+		if (num1 == null || num1.length() == 0 ||num1 == null || num1.length() == 0 || num2 == null || num2.length() == 0) {
+			showNotEnterdError(out);
+			return;
+		}
 
-		int i1 = Integer.parseInt(num1);
-		int i2 = Integer.parseInt(num2);
-		int answer = i1 + i2;
+		int answer = 0;
+		try {
+			int i1 = Integer.parseInt(num1);
+			int i2 = Integer.parseInt(num2);
+			answer = i1 + i2;
+		}catch(NumberFormatException e){
+			showNotIntegerError(out);
+			return;
+		}
 
 		out.println("<html><head><title>Plus</title></head><body>");
 		out.println(num1 + "+" + num2 + "=" + answer);
 		out.println("</body></html>");
 
+	}
+
+	private void showNotIntegerError(PrintWriter out) {
+		// TODO 自動生成されたメソッド・スタブ
+		out.println("<html><head><title>Plus</title></head><body>");
+		out.println("<h1>整数ではない値が入力されました</h1>");
+		out.println("</body></html>");
+	}
+
+	private void showNotEnterdError(PrintWriter out) {
+		// TODO 自動生成されたメソッド・スタブ
+		out.println("<html><head><title>Plus</title></head><body>");
+		out.println("<h1>整数を２つ入力してください</h1>");
+		out.println("</body></html>");
 	}
 
 }
