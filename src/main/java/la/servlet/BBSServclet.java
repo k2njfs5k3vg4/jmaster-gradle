a152b686ab3573eb29acecf63dd1922bd905e681
Shota Murano <dcmsd09072241242@gmail.com>
2020-06-11 11:09:20 +0900

    セッション管理 追加

--- src/main/java/la/servlet/BBSServclet.java
+++ src/main/java/la/servlet/BBSServclet.java
@@ -3,13 +3,13 @@ package la.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class BBSServclet
@@ -17,13 +17,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/BBSServclet")
 public class BBSServclet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	List<String> list = new ArrayList<String>();
-	List<String> store = new ArrayList<String>();
+	//	List<String> list = new ArrayList<String>();
+	//	List<String> store = new ArrayList<String>();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		String s = request.getParameter("message");
+
+		//セッション領域の取得
+		HttpSession session = request.getSession();
+		@SuppressWarnings("unchecked")
+		ArrayList<String> list = (ArrayList<String>) session.getAttribute("msg");
+		if (list == null) {
+			list = new ArrayList<String>();
+			session.setAttribute("msg", list);
+		}
 		list.add(s);
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
@@ -32,18 +41,18 @@ public class BBSServclet extends HttpServlet {
 		out.println("<form action='/jmaster-gradle/BBSServclet' method='get'>");
 		out.println("<textarea name='message'>入力してください</textarea><br>");
 		out.println("<input type = 'submit' value='書き込み'></form>");
-		out.println("<form action='/jmaster-gradle/BBSServclet' method='post'>");
-		out.println("<input type = 'submit' value='削除'></form>");
+		//		out.println("<form action='/jmaster-gradle/BBSServclet' method='post'>");
+		//		out.println("<input type = 'submit' value='削除'></form>");
 
-		for (String date : list) {
-			if (date == null) {
+		for (String data : list) {
+			if (data == null) {
 			} else {
-				if (date.equals("hello")) {
+				if (data.equals("hello")) {
 					out.println("<hr align='left' width='100%'>");
-					out.println("<font color='red'>" + date + "</font><br>");
+					out.println("<font color='red'>" + data + "</font><br>");
 				} else {
 					out.println("<hr align='left' width='100%'>");
-					out.println("<d>" + date + "</d><br>");
+					out.println("<d>" + data + "</d><br>");
 				}
 
 			}
@@ -51,34 +60,34 @@ public class BBSServclet extends HttpServlet {
 		out.println("</body></html>");
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		System.out.println("list（変更前）" + list);
-		int size = list.size() - 2;
-		if (size >= 0) {
-			store.clear();
-			for (String date : list) {
-				if (date != null) {
-					store.add(date);
-				}
-			}
-			store.remove(size);
-			list.clear();
-
-			for (String date2 : store) {
-				if (date2 != null) {
-					list.add(date2);
-				}
-			}
-
-			System.out.println("list（変更後）" + list);
-			System.out.println("store" + store);
-			System.out.println(size);
-		} else {
-			store.clear();
-			list.clear();
-		}
-		this.doGet(request, response);
-	}
+	//	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	//			throws ServletException, IOException {
+	//		System.out.println("list（変更前）" + list);
+	//		int size = list.size() - 2;
+	//		if (size >= 0) {
+	//			store.clear();
+	//			for (String data : list) {
+	//				if (data != null) {
+	//					store.add(data);
+	//				}
+	//			}
+	//			store.remove(size);
+	//			list.clear();
+	//
+	//			for (String data2 : store) {
+	//				if (data2 != null) {
+	//					list.add(data2);
+	//				}
+	//			}
+	//
+	//			System.out.println("list（変更後）" + list);
+	//			System.out.println("store" + store);
+	//			System.out.println(size);
+	//		} else {
+	//			store.clear();
+	//			list.clear();
+	//		}
+	//		this.doGet(request, response);
+	//	}
 
 }
