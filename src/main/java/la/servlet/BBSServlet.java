82b314b6c642584076acfd0272d6bf5fd7fc5ca0
Takaki Kawamura <docosys.r2kawamura@gmail.com>
2020-06-11 15:13:58 +0900

    re commit

--- src/main/java/la/servlet/BBSServlet.java
+++ src/main/java/la/servlet/BBSServlet.java
@@ -20,7 +20,6 @@ public class BBSServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 
 		response.setContentType("text/html;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
--- src/main/java/la/servlet/LoginServlet.java
+++ src/main/java/la/servlet/LoginServlet.java
@@ -15,11 +15,10 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
 	private static final String[] USER = { "kawamura", "hosodo" };
 	private static final String[] PASS = { "himitu", "tumihi" };
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.setContentType("text/html; charset=UTF-8");
@@ -74,10 +73,9 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doPost(request, response);
 	}
 
 }
