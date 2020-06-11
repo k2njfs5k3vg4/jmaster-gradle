c8281e8b1d7f2753694d32b6cad317cc05bcdab5
Makoto Takei <makoto.takei.tm@nttdocomo.com>
2020-06-11 17:17:53 +0900

    commit3

---
+++ src/main/java/la/servlet/PlusServlet4.java
@@ -0,0 +1,57 @@
+package la.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PlusServlet
+ */
+@WebServlet("/PlusServlet")
+public class PlusServlet4 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PlusServlet4() {
+		super();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		String num1 = request.getParameter("value1");
+		String num2 = request.getParameter("value2");
+
+		int i1 = Integer.parseInt(num1);
+		int i2 = Integer.parseInt(num2);
+		int answer = i1 + i2;
+
+
+		request.setAttribute("data1", Integer.valueOf(i1));
+		request.setAttribute("data2", Integer.valueOf(i2));
+		request.setAttribute("answer", Integer.valueOf(answer));
+
+		RequestDispatcher rd = request.getRequestDispatcher("/AnserServlet");
+		rd.forward(request, response);
+	}
+
+
+}
