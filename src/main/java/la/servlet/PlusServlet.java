6d429588166bfbd7136902f5be9534fb97032f2c
Hiroki Kameda <kmhrds@gmail.com>
2020-06-11 17:30:47 +0900

    PlusServletにリクエストスコープを追加

---
+++ src/main/java/la/servlet/PlusServlet.java
@@ -0,0 +1,100 @@
+package la.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PlusServlet
+ */
+@WebServlet("/PlusServlet")
+public class PlusServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public PlusServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		//リクエストパラメータの読み込み
+		String num1 = request.getParameter("value1");
+		String num2 = request.getParameter("value2");
+		if (num1 == null || num1.length() == 0 || num2 == null
+				|| num2.length() == 0) {
+			//未入力データあり
+			showNotEnterdError(out);
+			return;
+		}
+
+		//足し算の実行
+		int i1 = 0;
+		int i2 = 0;
+		int answer = 0;
+		try {
+			i1 = Integer.parseInt(num1);
+			i2 = Integer.parseInt(num2);
+			answer = i1 + i2;
+
+		} catch (NumberFormatException e) {
+			//整数でない
+			showNotIntegerError(out);
+			return;
+		}
+
+		//リクエストスコープにデータを入れる
+		request.setAttribute("data1", Integer.valueOf(i1));
+		request.setAttribute("data2", Integer.valueOf(i2));
+		request.setAttribute("answer", Integer.valueOf(answer));
+
+		//リクエストをAnswerServletに転送
+		RequestDispatcher rd = request.getRequestDispatcher("/AnswerServlet");
+		rd.forward(request, response);
+
+		//		//答えの出力
+		//		out.println("<html><head><title>Plus</title></head><body>");
+		//		out.println(num1 + "+" + num2 + "=" + answer);
+		//		out.println("</body></html>");
+		//		//doGet(request, response);
+	}
+
+	private void showNotIntegerError(PrintWriter out) {
+		out.println("<html><head><title>Plus</title></head><body>");
+		out.println("<h1>整数ではない値が入力されました</h1>");
+		out.println("</body></html>");
+	}
+
+	private void showNotEnterdError(PrintWriter out) {
+		out.println("<html><head><title>Plus</title></head><body>");
+		out.println("<h1>整数を二つ入力してください</h1>");
+		out.println("</body></html>");
+
+	}
+
+}
