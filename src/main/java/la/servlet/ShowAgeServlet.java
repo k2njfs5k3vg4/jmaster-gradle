3eb2f767d4750688547241d087622b6d4d01eeb2
Mayu Hosodo <dcmsd08095540442@gmail.com>
2020-06-12 15:11:12 +0900

    create Age

---
+++ src/main/java/la/servlet/ShowAgeServlet.java
@@ -0,0 +1,86 @@
+package la.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.servlet.*;
+
+/**
+ * Servlet implementation class ShoAgeServlet
+ */
+@WebServlet("/ShoAgeServlet")
+
+public class ShowAgeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ShowAgeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+
+		String name = request.getParameter("name");
+		String strYear = request.getParameter("year");
+		String strMonth = request.getParameter("month");
+		String strDate = request.getParameter("date");
+
+		if (strYear == null || strYear.length() == 0 || strMonth == null
+				|| strMonth.length() == 0 || strDate == null || strDate.length() == 0) {
+			request.setAttribute("message", "生年月日を入力してください");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInput.jsp");
+			rd.forward(request, response);
+			return;
+		}
+
+		int year, month, date;
+		try {
+			year = Integer.parseInt(strYear);
+			month = Integer.parseInt(strMonth);
+			date = Integer.parseInt(strDate);
+		} catch (NumberFormatException e) {
+			request.setAttribute("message", "生年月日は数値を入力してください");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInput.jsp");
+			rd.forward(request, response);
+			return;
+
+		}
+
+		//modelで計算
+		CalcAge ca = new CalcAge();
+		int age = ca.howOld(year, month, date);
+
+		//表示をJAVABEANS
+		PersonBean p = new PersonBean(name, age);
+		RequestDispatcher rd = request.getRequestDispatcher("/showAge.jsp");
+		rd.forward(request, response);
+		request.setAttribute("person", p);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
