42830a0c07573df3702ec9fb5872fbeed6ad3e23
SaranNakai <student@WIN-4K7MMDD9A76>
2020-06-12 10:19:11 +0900

    a

---
+++ src/main/java/la/servlet/AnswerServlet.java
@@ -0,0 +1,84 @@
+package la.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnswerServlet
+ */
+@WebServlet("/AnswerServlet")
+public class AnswerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AnswerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+}
+package la.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AnswerServlet
+ */
+@WebServlet("/AnswerServlet")
+public class AnswerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AnswerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		Integer num1 = (Integer) request.getAttribute("data1");
+		Integer num2 = (Integer) request.getAttribute("data2");
+		Integer answer = (Integer) request.getAttribute("answer");
+
+		out.println("<html><head><title>Plus</title></answer><body>");
+		out.println(num1 + "+" + num2 + "=" + answer);
+		out.println("</body></html>");
+	}
+
+}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
