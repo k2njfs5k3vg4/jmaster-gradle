24a32757bdd48c1fae9d8700794e2d3da5a491e5
SaranNakai <student@WIN-4K7MMDD9A76>
2020-06-12 10:32:34 +0900

    add plus

--- src/main/java/la/servlet/AnswerServlet.java
+++ src/main/java/la/servlet/AnswerServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import la.bean.PlusBean;
+
 /**
  * Servlet implementation class AnswerServlet
  */
@@ -16,30 +18,29 @@ import javax.servlet.http.HttpServletResponse;
 public class AnswerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AnswerServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
-		String answer = request.getParameter("answer");
 		PrintWriter out = response.getWriter();
-		//		request.setCharacterEncoding("UTF-8");
 
-		switch (answer) {
-		case "1":
-			out.println("<html><head><meta charset='UTF-8'><title>クイズの結果</title></head>");
-			out.println("<body><h1>不正解！</h1></body></html>");
-			break;
-		case "2":
-			out.println("<html><head><meta charset='UTF-8'><title>クイズの結果</title></head>");
-			out.println("<body><h1>正解！</h1></body></html>");
-			//			System.out.println("aaa");
-			break;
-		case "3":
-			out.println("<html><head><meta charset='UTF-8'><title>クイズの結果</title></head>");
-			out.println("<body><h1>不正解！</h1></body></html>");
-			break;
-		default:
-			break;
-		}
+		//		Integer num1 = (Integer) request.getAttribute("data1");
+		//		Integer num2 = (Integer) request.getAttribute("data2");
+		//		Integer answer = (Integer) request.getAttribute("answer");
+
+		PlusBean bean = (PlusBean) request.getAttribute("plus");
+
+		out.println("<html><head><title>Plus</title></answer><body>");
+		//		out.println(num1 + "+" + num2 + "=" + answer);
+		out.println(bean.getValue1() + "+" + bean.getValue2() + "=" + bean.getAnswer());
+		out.println("</body></html>");
 	}
 
 }
