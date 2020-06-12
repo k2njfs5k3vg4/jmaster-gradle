bf63b723e04c1cb7f3d3fee32968c0ffb9c6ec80
Liang <08022789396@docomo.ne.jp>
2020-06-12 15:34:10 +0900

    final version

--- src/main/java/la/servlet/ShowAgeServlet.java
+++ src/main/java/la/servlet/ShowAgeServlet.java
@@ -9,9 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import la.servlet.PersonBean;
-//import la.servlet.CalcAge;
-
 /**
  * Servlet implementation class ShowAgeServlet
  */
