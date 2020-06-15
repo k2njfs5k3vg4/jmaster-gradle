ac9312856e8ec22e3544f72b5fe297809eb556a5
Liang <08022789396@docomo.ne.jp>
2020-06-15 17:25:05 +0900

    add EmployeeServlet

---
+++ src/main/java/la/servlet/EmployeeServlet.java
@@ -0,0 +1,63 @@
+package la.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.bean.EmployeeBean;
+import la.dao.DAOException;
+import la.dao.EmployeeDAO;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+@WebServlet("/EmployeeServlet")
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EmployeeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			EmployeeDAO dao = new EmployeeDAO();
+			List<EmployeeBean> list = dao.findAll();
+
+			request.setAttribute("employees", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/showEmployee.jsp");
+			rd.forward(request, response);
+		} catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました。");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInternal.jsp");
+			rd.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
