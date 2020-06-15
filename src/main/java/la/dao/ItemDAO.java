b9f68e4143690301b220e8f07f1aebec5c0db106
Hiroki Kameda <kmhrds@gmail.com>
2020-06-15 17:17:09 +0900

    modify itemDAO and Test

--- src/main/java/la/dao/ItemDAO.java
+++ src/main/java/la/dao/ItemDAO.java
@@ -2,12 +2,23 @@ package la.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import la.bean.ItemBean;
 
 public class ItemDAO {
 
 	private Connection con;
 
-	public  void getConnection() throws DAOException{
+	public ItemDAO() throws DAOException {
+		getConnection();
+	}
+
+	public void getConnection() throws DAOException {
 		try {
 			// 初期設定
 			String url = "jdbc:postgresql:sample";
@@ -16,10 +27,64 @@ public class ItemDAO {
 
 			// DBへの接続
 			con = DriverManager.getConnection(url, user, pass);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			throw new DAOException("接続に失敗");
 		}
 
+	}
+
+	public List<ItemBean> findAll() throws DAOException {
+		if (con == null) {
+			getConnection();
+		}
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			//SQL文の作成
+			String sql = "select * from emp";
+
+			//PreparedStatementオブジェクトの取得
+			st = con.prepareStatement(sql);
+			rs = st.executeQuery();
+
+			List<ItemBean> list = new ArrayList<ItemBean>();
+			while (rs.next()) {
+				int code = rs.getInt("code");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				String tel = rs.getString("tel");
+				ItemBean bean = new ItemBean(code, name, age, tel);
+				list.add(bean);
+			}
+			return list;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		} finally {
+			try {
+				//リソースの解放
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				close();
+
+			} catch (Exception e) {
+				throw new DAOException("リソースの解放に失敗しました。");
+			}
+		}
+	}
+
+	private void close() throws SQLException {
+		if (con != null) {
+			con.close();
+			con = null;
+		}
 	}
 
 }
--- src/test/java/la/dao/ItemDAOTest.java
+++ src/test/java/la/dao/ItemDAOTest.java
@@ -1,23 +1,41 @@
 package la.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import la.bean.ItemBean;
+
 class ItemDAOTest extends ItemDAO {
+
+	//	@Test
+	//	@DisplayName("getConnectionメソッド")
+	//	void test1() {
+	//		ItemDAO dao = new ItemDAO();
+	//		try {
+	//			dao.getConnection();
+	//			return;
+	//		}catch{
+	//			fail("まだ実装されていません");
+	//		}
+	//	}
+
+	public ItemDAOTest() throws DAOException {
+		super();
+	}
 
 	@Test
 	@DisplayName("getConnectionメソッド")
 	void test1() {
-		ItemDAO dao = new ItemDAO();
 		try {
-			dao.getConnection();
-			return;
-		}catch{
-			fail("まだ実装されていません");
+			ItemDAO dao = new ItemDAO();
+			List<ItemBean> list = dao.findAll();
+
+		} catch (DAOException e) {
+			e.printStackTrace();
 		}
+
 	}
 
 }
