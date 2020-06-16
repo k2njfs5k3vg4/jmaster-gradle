cb6d58f8b1f36085be287ebe89867ac9594c759b
Takaki Kawamura <docosys.r2kawamura@gmail.com>
2020-06-16 10:06:54 +0900

    commitEmplo2

---
+++ WebContent/EmployeeForm.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="/jmaster-gradle/EmployeeServlet2" method="post">
+番号：<input type="number" name="code"><br>
+名前：<input type="text" name="name"><br>
+年齢：<input type="number" name="age"><br>
+電話番号：<input type="text" name="tel"s><br>
+<input type="submit" value="追加">
+<input type="hidden" name="action" value="add">
+</form>
+</body>
+</html>
---
+++ src/main/java/la/servlet/EmployeeServlet2.java
@@ -0,0 +1,66 @@
+package la.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import la.bean.EmployeeBean;
+import la.dao.DAOException;
+import la.dao.EmployeeDAO2;
+
+/**
+ * Servlet implementation class EmployeeServlet2
+ */
+@WebServlet("/EmployeeServlet2")
+public class EmployeeServlet2 extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			request.setCharacterEncoding("UTF-8");
+
+			String action = request.getParameter("action");
+
+			EmployeeDAO2 dao = new EmployeeDAO2();
+			if (action.contentEquals("add")) {
+				int code = Integer.parseInt(request.getParameter("code"));
+				String name = request.getParameter("name");
+				int age = Integer.parseInt(request.getParameter("age"));
+				String tel = request.getParameter("tel");
+				dao.addEmployee(code, name, age, tel);
+				List<EmployeeBean> list = dao.findAll();
+				request.setAttribute("employees", list);
+				gotoPage(request, response, "/showEmployee2.jsp");
+			}
+
+		} catch (DAOException e) {
+			e.printStackTrace();
+			request.setAttribute("message", "内部エラーが発生しました");
+			gotoPage(request, response, "/errInternal.jsp");
+
+		}
+
+	}
+
+	private void gotoPage(HttpServletRequest request, HttpServletResponse response, String page)
+			throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(page);
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
